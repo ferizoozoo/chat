ad82e3e320a1113268567d9f30b91abc21e20b79
ferizoozoo <farhadzohoorz@gmail.com>
2024-11-17 16:08:45 +0330

    refactor: add title field to room schema

--- server/models/room.js
+++ server/models/room.js
@@ -4,7 +4,11 @@ const roomSchema = new mongoose.Schema({
     members: [{
         type: Schema.Types.ObjectId,
         ref: 'User'
-    }]
+    }],
+    title: {
+        type: String,
+        required: true
+    },
 }, {
     timestamps: {
         createdAt: 'created_at',
