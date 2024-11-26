72227e28278db0459bac1b7b2127c6a606b5caf9
ferizoozoo <farhadzohoorz@gmail.com>
2024-11-26 21:55:55 +0330

    fix: make updated localstorage available after updating the value

--- client/src/hooks/useLocalStorage.ts
+++ client/src/hooks/useLocalStorage.ts
@@ -1,22 +1,49 @@
 import { useEffect, useState } from "react";
 
-function useLocalStorage(
+const useLocalStorage = (
   key: string,
   initialValue: string = ""
-): [string | null, (x: string) => void] {
+): [string | null, (x: string) => void] => {
   const [value, setValue] = useState(() => {
     if (typeof window !== "undefined") {
-      return localStorage.getItem(key);
+      return localStorage.getItem(key) || initialValue;
     }
     return initialValue;
   });
 
   const updateLocalStorage = (item: string) => {
-    localStorage.setItem(key, item);
-    setValue(item);
+    if (!item) {
+      localStorage.removeItem(key);
+      setValue(null);
+    } else {
+      localStorage.setItem(key, item);
+      setValue(item);
+    }
+
+    window.dispatchEvent(
+      new CustomEvent(`localStorageUpdate:${key}`, { detail: item })
+    );
   };
 
+  useEffect(() => {
+    const handleStorageChange = (e: CustomEvent) => {
+      setValue(e.detail);
+    };
+
+    window.addEventListener(
+      `localStorageUpdate:${key}`,
+      handleStorageChange as EventListener
+    );
+
+    return () => {
+      window.removeEventListener(
+        `localStorageUpdate:${key}`,
+        handleStorageChange as EventListener
+      );
+    };
+  }, [key]);
+
   return [value, updateLocalStorage];
-}
+};
 
 export default useLocalStorage;
